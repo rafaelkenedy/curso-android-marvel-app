249e294aeb25c5f60ac63ee181316ae06de0705b
rafael <k.rafaelalves@gmail.com>
2022-11-30 14:17:56 -0300

    Implementa repositório na camada de framework

---
+++ app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.marvelapp.framework
+
+import androidx.paging.PagingSource
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.rafael.core.data.repository.CharactersRemoteDataSource
+import com.rafael.core.data.repository.CharactersRepository
+import com.rafael.core.domain.model.Character
+import javax.inject.Inject
+
+class CharactersRepositoryImpl @Inject constructor(
+    private val remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
+) : CharactersRepository{
+
+    override fun getCharacter(query: String): PagingSource<Int, Character> {
+        return CharactersPaging()
+    }
+}
