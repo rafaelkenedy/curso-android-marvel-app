0be8a8f060f6bc9624509d7f360eb5f9da876386
rafael <k.rafaelalves@gmail.com>
2022-12-01 14:01:39 -0300

    Implementa paging

--- app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.marvelapp.framework
 
 import androidx.paging.PagingSource
 import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.framework.paging.CharactersPagingSource
 import com.rafael.core.data.repository.CharactersRemoteDataSource
 import com.rafael.core.data.repository.CharactersRepository
 import com.rafael.core.domain.model.Character
@@ -9,9 +10,9 @@ import javax.inject.Inject
 
 class CharactersRepositoryImpl @Inject constructor(
     private val remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
-) : CharactersRepository{
+) : CharactersRepository {
 
     override fun getCharacter(query: String): PagingSource<Int, Character> {
-        return CharactersPaging()
+        return CharactersPagingSource(remoteDataSource, query)
     }
 }
