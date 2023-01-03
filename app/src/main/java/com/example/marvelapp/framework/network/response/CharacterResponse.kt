e710f907b8db03bdc5d79eb4e654d493e5629057
rafael <k.rafaelalves@gmail.com>
2023-01-03 09:11:33 -0300

    Implementa build variants

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,10 +1,14 @@
 package com.example.marvelapp.framework.network.response
 
+import com.google.gson.annotations.SerializedName
 import com.rafael.core.domain.model.Character
 
 data class CharacterResponse(
+    @SerializedName("id")
     val id: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("thumbnail")
     val thumbnail: ThumbnailResponse
 )
 
